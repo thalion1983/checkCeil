8b8bc8bb834cc4a24eb25f351fc533282dffc33a
Leonardo Astros <leonardoastros@gmail.com>
2023-12-26 20:29:38 -0400

    [ADD] Adding Ceil calculation without converting to float

--- main.go
+++ main.go
@@ -14,8 +14,14 @@ func main() {
 		noConv  := a / b                                   // No conversion at all
 		oneConv := int(math.Ceil(float64(a / b)))          // Convert the result of division to float64
 		twoConv := int(math.Ceil(float64(a) / float64(b))) // Convert a and b to float64 before division
+		// Got Ceil without math and without conversion
+		intCeil := a / b
+		if a % b != 0 {
+			intCeil += 1
+		}
 		fmt.Printf("              No float conversion: %d\n", noConv)
 		fmt.Printf("        Division result converted: %d\n", oneConv)
-		fmt.Printf("a and b converted before division: %d\n\n", twoConv)
+		fmt.Printf("a and b converted before division: %d\n", twoConv)
+		fmt.Printf("         ceil without conversions: %d\n\n", intCeil)
 	}
 }
